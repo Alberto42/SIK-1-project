ab8ac319392b6c3ee2de3e7d4366f50abcc08c6e
Albert Cieslak <albert.cieslak2@gmail.com>
2018-04-24 21:45:58 +0200

    refactor

--- server.cpp
+++ server.cpp
@@ -5,53 +5,34 @@
 #include <string.h>
 #include <unistd.h>
 #include <cstdlib>
+#include <ctype.h>
 
 #include "err.h"
+#include "server.h"
 
 #define BUFFER_SIZE   2000
 #define QUEUE_LENGTH     5
 
+int sock,port;
+int msg_sock;
+sockaddr_in client_address;
+socklen_t client_address_len;
+char buffer[2000];
+ssize_t len;
+ssize_t snd_len;
+
 int main(int argc, char *argv[]) {
-    int sock, msg_sock;
-    struct sockaddr_in server_address;
-    struct sockaddr_in client_address;
-    socklen_t client_address_len;
 
-    char buffer[BUFFER_SIZE];
-    ssize_t len, snd_len;
+    check_args(argc, argv);
 
-    sock = socket(PF_INET, SOCK_STREAM, 0); // creating IPv4 TCP socket
-    if (sock < 0)
-        syserr("socket");
-    // after socket() call; we should close(sock) on any execution path;
-    // since all execution paths exit immediately, sock would be closed when program terminates
+    prepare_socket();
 
-    server_address.sin_family = AF_INET; // IPv4
-    server_address.sin_addr.s_addr = htonl(
-            INADDR_ANY); // listening on all interfaces
-    server_address.sin_port = htons(atoi(argv[1])); // listening on port PORT_NUM
-
-    // bind the socket to a concrete address
-    if (bind(sock, (struct sockaddr *) &server_address,
-             sizeof(server_address)) < 0)
-        syserr("bind");
-
-    // switch to listening (passive open)
-    if (listen(sock, QUEUE_LENGTH) < 0)
-        syserr("listen");
-
-    printf("accepting client connections on port %hu\n",
-           ntohs(server_address.sin_port));
     for (;;) {
-        client_address_len = sizeof(client_address);
-        // get client connection from the socket
-        msg_sock = accept(sock, (struct sockaddr *) &client_address,
-                          &client_address_len);
-        if (msg_sock < 0)
-            syserr("accept");
+        wait_for_client();
         char negotiation_message[] = {(char)255,(char)251,(char)1,
                                       (char)255,(char)251,(char)3,
                                       (char)255,(char)252,(char)34,(char)0};
+
         strcpy(buffer,negotiation_message);
         snd_len = write(msg_sock, buffer, strlen(negotiation_message));
         if (snd_len != strlen(negotiation_message))
@@ -74,4 +55,57 @@ int main(int argc, char *argv[]) {
     }
 
     return 0;
+}
+
+void wait_for_client() {
+    client_address_len = sizeof(client_address);
+    // get client connection from the socket
+    msg_sock = accept(sock, (struct sockaddr *) &client_address,
+                          &client_address_len);
+    if (msg_sock < 0)
+            syserr("accept");
+}
+
+void check_args(int argc, char *const *argv) {
+    if (argc != 2) {
+        printf("Usage: ./server <port> \n");
+        exit(-1);
+    }
+    port = atoi(argv[1]);
+    if (port < 0 || port > 65536) {
+        printf("Usage: ./server <port> \n");
+        exit(-1);
+    }
+}
+
+void prepare_socket() {
+    int msg_sock;
+    struct sockaddr_in server_address;
+    socklen_t client_address_len;
+
+    char buffer[BUFFER_SIZE];
+    ssize_t len, snd_len;
+
+    sock = socket(PF_INET, SOCK_STREAM, 0); // creating IPv4 TCP socket
+    if (sock < 0)
+        syserr("socket");
+    // after socket() call; we should close(sock) on any execution path;
+    // since all execution paths exit immediately, sock would be closed when program terminates
+
+    server_address.sin_family = AF_INET; // IPv4
+    server_address.sin_addr.s_addr = htonl(
+            INADDR_ANY); // listening on all interfaces
+    server_address.sin_port = htons(port); // listening on port PORT_NUM
+
+    // bind the socket to a concrete address
+    if (bind(sock, (struct sockaddr *) &server_address,
+             sizeof(server_address)) < 0)
+        syserr("bind");
+
+    // switch to listening (passive open)
+    if (listen(sock, QUEUE_LENGTH) < 0)
+        syserr("listen");
+
+    printf("accepting client connections on port %hu\n",
+           ntohs(server_address.sin_port));
 }
---
+++ server.h
@@ -0,0 +1,13 @@
+//
+// Created by albert on 24.04.18.
+//
+
+#ifndef SIK_ZADANIE_1_SERVER_H
+#define SIK_ZADANIE_1_SERVER_H
+
+void prepare_socket();
+
+void check_args(int argc, char *const *argv);
+
+void wait_for_client();
+#endif //SIK_ZADANIE_1_SERVER_H
